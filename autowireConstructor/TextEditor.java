db648f4bbab80b7c7178164c5ba1cbec1306584a
Chaitra111 <111chaitra@gmail.com>
2018-07-03 17:13:53 +0530

    commit

---
+++ autowireConstructor/TextEditor.java
@@ -0,0 +1,27 @@
+package com.Spring_1.autowireConstructor;
+
+public class TextEditor {
+
+	private SpellChecker spellChecker;
+	private String name;
+
+	public void setSpellChecker(SpellChecker spellChecker) {
+		this.spellChecker = spellChecker;
+	}
+
+	public SpellChecker getSpellChecker() {
+		    return spellChecker;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void spellCheck() {
+		spellChecker.checkSpelling();
+	}
+}
