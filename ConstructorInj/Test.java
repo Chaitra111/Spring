088a3243c3f894a6929848f7208dc3e6ba6a0c6a
Chaitra111 <111chaitra@gmail.com>
2018-07-03 17:39:04 +0530

    add

---
+++ ConstructorInj/Test.java
@@ -0,0 +1,20 @@
+package com.Spring.ConstructorInj;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+@SuppressWarnings("deprecation")
+public class Test {
+
+	public static void main(String[] args) {  
+        
+        Resource r=new ClassPathResource("applicationContext1.xml");  
+        BeanFactory factory=new XmlBeanFactory(r);  
+          
+        Employee s=(Employee)factory.getBean("e");  
+        s.show();  
+          
+    }  
+}  
